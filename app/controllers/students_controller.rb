f0890c0e06472df4eca87642104cd1b0d6e9dcbf
mcardona9015 <mcardona9015@gmail.com>
2021-01-12 22:18:43 -0500

    commit tree a415fdc8803827083e9bd39ad4ccc64bbb54a678

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,21 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @students = Student.all
+    @student = Student.new
+
+  end
+
+  def create
+    new_student = Student.create(student_params)
+    redirect_to "/students/#{new_student.id}"
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
 end
